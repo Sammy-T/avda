2fa1c5e7e394213451c84c716c746d69d13dffaa
Sammy-T <cyam9019@gmail.com>
2024-07-08 03:30:56 -0400

    Create "select vault" modal

--- frontend/src/lib/App.svelte
+++ frontend/src/lib/App.svelte
@@ -1,5 +1,8 @@
 <script>
     import Main from '$lib/pages/Main.svelte';
+    import SelectVault from './modals/SelectVault.svelte';
 </script>
 
 <Main />
+
+<SelectVault />
---
+++ frontend/src/lib/modals/SelectVault.svelte
@@ -0,0 +1,37 @@
+<dialog open>
+    <article>
+        <h1>avdap</h1>
+
+        <h5>Select vault file</h5>
+        <form>
+            <input type="file" />
+
+            <input type="password" name="password" placeholder="Password" />
+            <small>Leave blank for unencrypted vault files.</small>
+
+            <button>Unlock</button>
+        </form>
+    </article>
+</dialog>
+
+<style>
+    article {
+        max-width: 500px;
+        padding: calc(var(--pico-spacing) * 2);
+        background-image: linear-gradient(135deg, #d7e3f4ff, #8f9bc9ff);
+    }
+
+    h1 {
+        text-align: center;
+        margin-bottom: calc(var(--pico-typography-spacing-vertical) * 2.5);
+        color: var(--pico-primary);
+    }
+
+    h1, h5 {
+        font-weight: 400;
+    }
+
+    form > button {
+        width: 100%;
+    }
+</style>
