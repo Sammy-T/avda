7eeaacce3037c349e8cd59712d612ab6de674fc9
Sammy-T <cyam9019@gmail.com>
2024-07-09 21:55:09 -0400

    Set app width

--- main.go
+++ main.go
@@ -18,7 +18,7 @@ func main() {
 	// Create application with options
 	err := wails.Run(&options.App{
 		Title:  "avda",
-		Width:  1024,
+		Width:  720,
 		Height: 768,
 		AssetServer: &assetserver.Options{
 			Assets: assets,
