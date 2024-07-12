50d9afdf0f88542cff10e92d9721ce469ccb4e46
Sammy-T <cyam9019@gmail.com>
2024-07-12 19:49:47 -0400

    Update app version

--- frontend/src/lib/components/Footer.svelte
+++ frontend/src/lib/components/Footer.svelte
@@ -26,7 +26,7 @@ function openExtUrl(event) {
         </ul>
 
         <ul>
-            <li><small>v1.0.0</small></li>
+            <li><small>v1.1.0</small></li>
         </ul>
     </nav>
 </footer>
