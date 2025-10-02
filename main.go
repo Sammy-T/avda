d512c1abf3b6f571011645dfabf8be66f0ae27bc
Sammy-T <cyam9019@gmail.com>
2025-10-01 23:24:27 -0400

    Set linux program name

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"github.com/wailsapp/wails/v2"
 	"github.com/wailsapp/wails/v2/pkg/options"
 	"github.com/wailsapp/wails/v2/pkg/options/assetserver"
+	"github.com/wailsapp/wails/v2/pkg/options/linux"
 )
 
 //go:embed all:frontend/dist
@@ -31,6 +32,9 @@ func main() {
 		OnStartup:                app.startup,
 		Bind: []any{
 			app,
+		},
+		Linux: &linux.Options{
+			ProgramName: "com.sammy-t.avda",
 		},
 	})
 
